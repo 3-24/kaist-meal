1da55a538456ccc0d03691a224b59a9144b508b9
Youngseok Choe <dev.youngseok@gmail.com>
2022-05-03 20:02:34 +0900

    Cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,6 @@ use std::env;
 use serenity::async_trait;
 use serenity::model::gateway::Ready;
 use serenity::model::id::GuildId;
-use serenity::model::interactions::application_command::{
-    ApplicationCommand,
-    ApplicationCommandInteractionDataOptionValue,
-    ApplicationCommandOptionType,
-};
 use serenity::model::interactions::{Interaction, InteractionResponseType};
 use serenity::prelude::*;
 
@@ -16,13 +11,12 @@ use scraper::{Html, Selector};
 
 use chrono::prelude::*;
 
-
 struct Handler;
 
 enum MealTime {
     Breakfast,
     Lunch,
-    Dinner
+    Dinner,
 }
 
 async fn parse_meal(location: &str, meal_time: MealTime) -> String {
@@ -33,26 +27,39 @@ async fn parse_meal(location: &str, meal_time: MealTime) -> String {
             panic!()
         }
     };
-    let query_url = format!("https://www.kaist.ac.kr/kr/html/campus/053001.html?dvs_cd={}", loc_get_param);
-    let resp = reqwest::get(query_url).await.expect("await fail").text().await.expect("await fail");
+    let query_url = format!(
+        "https://www.kaist.ac.kr/kr/html/campus/053001.html?dvs_cd={}",
+        loc_get_param
+    );
+    let resp = reqwest::get(query_url)
+        .await
+        .expect("await fail")
+        .text()
+        .await
+        .expect("await fail");
     let parsed_html = Html::parse_document(&resp);
-    
+
     let css_selector = match meal_time {
         MealTime::Breakfast => "#tab_item_1 > table > tbody > tr > td:nth-child(1) > ul",
         MealTime::Lunch => "#tab_item_1 > table > tbody > tr > td:nth-child(2) > ul",
-        MealTime::Dinner => "#tab_item_1 > table > tbody > tr > td:nth-child(3) > ul"
+        MealTime::Dinner => "#tab_item_1 > table > tbody > tr > td:nth-child(3) > ul",
     };
     let selector = &Selector::parse(css_selector).expect("Error during the paring using selector");
-    let parsed_result = parsed_html.select(selector).next()
-    .expect("Error during selection").inner_html()
-    .replace("<br>", "")
-    .replace("&amp", "&");
+    let parsed_result = parsed_html
+        .select(selector)
+        .next()
+        .expect("Error during selection")
+        .inner_html()
+        .replace("<br>", "")
+        .replace("&amp", "&");
 
     parsed_result
 }
 
 fn get_current_time() -> MealTime {
-    let local = Utc::now().with_timezone(&FixedOffset::east( 3600*9 )).time(); // KST
+    let local = Utc::now()
+        .with_timezone(&FixedOffset::east(3600 * 9))
+        .time(); // KST
 
     let meal_cut = NaiveTime::from_hms(13, 30, 0);
     if local > meal_cut {
@@ -62,7 +69,6 @@ fn get_current_time() -> MealTime {
     }
 }
 
-
 #[async_trait]
 impl EventHandler for Handler {
     async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
@@ -70,9 +76,9 @@ impl EventHandler for Handler {
             let command_name = command.data.name.as_str();
             let content = match command_name {
                 "카이마루" | "교수회관" => {
-                    let result:String = parse_meal(command_name, get_current_time()).await;
+                    let result: String = parse_meal(command_name, get_current_time()).await;
                     result
-                },
+                }
                 _ => "not implemented :(".to_string(),
             };
 
@@ -101,16 +107,15 @@ impl EventHandler for Handler {
 
         let commands = GuildId::set_application_commands(&guild_id, &ctx.http, |commands| {
             commands
-                .create_application_command(|command| {
-                    command.name("카이마루").description("밥")
-                })
-                .create_application_command(|command| {
-                    command.name("교수회관").description("바압")
-                })
+                .create_application_command(|command| command.name("카이마루").description("밥"))
+                .create_application_command(|command| command.name("교수회관").description("바압"))
         })
         .await;
 
-        println!("I now have the following guild slash commands: {:#?}", commands);
+        println!(
+            "I now have the following guild slash commands: {:#?}",
+            commands
+        );
         /*
         let guild_command =
             ApplicationCommand::create_global_application_command(&ctx.http, |command| {
